24c0a5ec22a581168c29731d4d1647a1afbc4145
Nataniel Schling <natanielschling@gmail.com>
2014-04-23 00:30:00 +0200

    Fully functional routine. 'Recursivity' implementation for teoretycally infinitum quantity.

--- js-numerals/script.js
+++ js-numerals/script.js
@@ -35,24 +35,25 @@ var tens = new Array(
 );
 var thousands = new Array(
  '',
- ' thousand',
- ' million',
- ' billion',
- ' trillion',
- ' quadrillion',
- ' quintillion',
- ' sextillion',
- ' septillion',
- ' octillion',
- ' nonillion'
+ 'thousand',
+ 'million',
+ 'billion',
+ 'trillion',
+ 'quadrillion',
+ 'quintillion',
+ 'sextillion',
+ 'septillion',
+ 'octillion',
+ 'nonillion'
 );
 
 
-arabic2en = function(v) {
+arabic2en = function(v, t) {
     var result = '';
 
     a = Math.round((v % 100 - 0.5));
-    b = Math.round((v / 100 ) - 0.5 ) % 10;
+    b = Math.round((v / 100) - 0.5) % 10;
+    r = Math.round((v / 1000) - 0.5);
 
     if(b > 0) { // hundreds
         result = ones[b] + ' hundred ';
@@ -62,6 +63,12 @@ arabic2en = function(v) {
         result += ones[a];
     } else {
         result += tens[Math.round((a / 10) - 0.5)] + ' ' + ones[a % 10];
+    }
+    if(result) { // thousands
+        result += thousands[t] + ' ';
+    }     
+    if(r > 0) { // recursivity if it is rest
+        return arabic2en(r, t + 1) + result;
     }
 
     return result;
