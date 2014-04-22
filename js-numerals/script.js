ae7b619358b6f4400e8819f8f274df856a809e99
Nataniel Schling <natanielschling@gmail.com>
2014-04-22 23:56:03 +0200

    Hundreds and thousands array structure.

--- js-numerals/script.js
+++ js-numerals/script.js
@@ -33,12 +33,30 @@ var tens = new Array(
  'eighty',
  'ninety'
 );
+var thousands = new Array(
+ '',
+ ' thousand',
+ ' million',
+ ' billion',
+ ' trillion',
+ ' quadrillion',
+ ' quintillion',
+ ' sextillion',
+ ' septillion',
+ ' octillion',
+ ' nonillion'
+);
 
 
 arabic2en = function(v) {
     var result = '';
 
-    a = Math.round(v % 100);
+    a = Math.round((v % 100 - 0.5));
+    b = Math.round((v / 100 ) - 0.5 ) % 10;
+
+    if(b > 0) { // hundreds
+        result = ones[b] + ' hundred ';
+    }
     //from 1 to 19
     if (a < 20) {
         result += ones[a];
