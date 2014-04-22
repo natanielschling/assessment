5028886374c65699b0e522514453a21c967fa5c2
Nataniel Schling <natanielschling@gmail.com>
2014-04-22 23:14:54 +0200

    Script temporary call, up to 19 ok.

--- js-numerals/script.js
+++ js-numerals/script.js
@@ -1,4 +1,36 @@
-arabic2en = function() {
-    var result = null;
+var ones = new Array(
+ '',
+ 'one',
+ 'two',
+ 'three',
+ 'four',
+ 'five',
+ 'six',
+ 'seven',
+ 'eight',
+ 'nine',
+ 'ten',
+ 'eleven',
+ 'twelve',
+ 'thirteen',
+ 'fourteen',
+ 'fifteen',
+ 'sixteen',
+ 'seventeen',
+ 'eighteen',
+ 'nineteen'
+);
+
+arabic2en = function(v) {
+    var result = '';
+
+    a = Math.round(v % 100);
+    //from 1 to 19
+    if (a < 20) {
+        result += ones[a];
+    } else {
+        result = 'not yet';
+    }
+
     return result;
 }
